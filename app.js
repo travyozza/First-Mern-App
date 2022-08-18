f46fd45154132dd14ccc8886cb22710ffd7a330c
travyozza <84102421+travyozza@users.noreply.github.com>
2022-08-18 13:46:39 +1200

    Local app

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const connectDB = require("./config/db");
+var cors = require("cors");
+
+const books = require("./routes/api/books");
 
 const app = express();
 
 connectDB();
 
+app.use(cors({ origin: true, credentials: true }));
+
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => res.send("Hello World!"));
+
+app.use("/api/books", books);
 
 const port = process.env.PORT || 8082;
 
